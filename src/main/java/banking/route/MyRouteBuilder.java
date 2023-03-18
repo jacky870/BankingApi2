13d4649089cb93744ce590889a076e38dad3ee80
ykcaj <ykcajtihs@hotmail.com>
2023-03-18 15:11:38 -0400

    just add it

---
+++ src/main/java/banking/route/MyRouteBuilder.java
@@ -0,0 +1,31 @@
+package banking.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component()
+public class MyRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        restConfiguration()
+                .component("jetty")
+                .contextPath("/")
+                .port(8080)
+                .enableCORS(true)
+                .corsAllowCredentials(true)
+                .corsHeaderProperty("Access-Control-Allow-Origin","*")
+                .corsHeaderProperty("Access-Control-Allow-Headers","Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization")
+        ;
+
+
+        rest("/users").enableCORS(true)
+                .get("/{id}").to("direct:getUser");
+
+        from("direct:getUser")
+                .routeId("getUserRoute")
+                .log("hello world")
+                .setBody(simple("hello world"))
+                .setHeader("CamelHttpResponseCode", constant(200));
+    }
+}
